fb7b024ca0277a51b64a1aa31200f9327e8bdcb9
Liam Huang <liam@Liams-MacBook-Pro.local>
2023-04-30 15:31:22 -0400

    add embedded swagger frontend doc

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ import (
 
 	"github.com/Liam501/SimpleBank/api"
 	db "github.com/Liam501/SimpleBank/db/sqlc"
+	_ "github.com/Liam501/SimpleBank/doc/statik"
 	"github.com/Liam501/SimpleBank/gapi"
 	"github.com/Liam501/SimpleBank/pb"
 	"github.com/Liam501/SimpleBank/util"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -85,8 +87,13 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./doc/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs:", err)
+	}
+
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
