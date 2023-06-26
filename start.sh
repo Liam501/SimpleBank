4cdbe86eec90bb60a79a410e75e53ecce3be0a36
Liam Huang <tony11037636@gmail.com>
2023-06-26 18:48:26 +0800

    test deploy 4

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e 
 
 echo "run db migration"
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+/app/migrate -path /app/migration -database "postgresql://root:apw5vkg!JND4jhk*bkr@simple-bank.cejknk3keljc.us-east-2.rds.amazonaws.com:5432/simple_bank" -verbose up
 
 echo "start the app"
 exec "$@"
